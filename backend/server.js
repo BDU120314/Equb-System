ba5662fe2463aa0b34825b1d93286408d8f7c956
MulukenDemis <amsaledemismuller@gmail.com>
2023-05-08 12:45:49 +0300

    muller 1st commit

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,15 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.get("/", 
-(res, req) =>{
+app.get("/", (req,res) =>{
   res.send("server js")
 
-  })
+  });
+
+  app.post("/muller",(req,res)=>{
+res.send("muluken");
+  });
+  
 
   app.listen(port, 
   () =>{
