440951316c3e935a9b7796e113225c1ff2291700
MulukenDemis <amsaledemismuller@gmail.com>
2023-05-09 01:26:45 +0300

    muller 4st commit

--- backend/model/equbUserModels.js
+++ backend/model/equbUserModels.js
@@ -4,22 +4,27 @@ const mongoose=require('mongoose');
 const Schema=mongoose.Schema;
 
 
-const userSchema=new Schema({
-      fullName: {
-    type: String,
-    required: true,
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-  address: {
-    type: String,
-    required: true,
-  },
- 
-},
- {timestamps:true})
+  { timestamps: true }
+);
 
 module.exports=mongoose.model('equbUser',userSchema);
 
