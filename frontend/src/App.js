06f412cd868fe6f642f58f071818346dc09d30c1
BDU120314 <tsecha6@gmail.com>
2023-05-18 22:51:07 +0300

    react with javascript

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+   hellwo world
+    </div>
+  );
+}
+
+export default App;
--- frontend/src/react-app-env.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="react-scripts" />
-/// <reference types="react-dom" /> 
-/// <reference types="react" />
