66dbe8e2ce8225ecd77ce78d696f556633bb1893
gebrehywot <gebreagumass@gmail.com>
2023-05-24 09:33:57 +0300

    login commited

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,54 +1,141 @@
-<<<<<<< HEAD
-import React from "react";
-import logo from "../assets/equb.png";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import axios from "axios"; // Import Axios library
+
 const Login = () => {
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isForgotPassword, setIsForgotPassword] = useState(false);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    if (isForgotPassword) {
+      if (email && email.length > 0) {
+        // Placeholder for forgot password logic using email
+        console.log("Password reset instructions sent to your email.");
+        // Perform the necessary actions after sending password reset instructions via email
+      } else {
+        console.log("Email is required for password reset.");
+        // Perform the necessary actions if email is not provided for password reset
+      }
+    } else if (password.length <= 6) {
+      setErrorMessage("Password must be greater than six characters.");
+    } else if (!validatePhoneNumber(phone)) {
+      setErrorMessage("Invalid phone number.");
+    } else {
+      // Placeholder for login logic using phone number and password
+      axios
+        .post("/login", { phone, password })
+        .then((response) => {
+          if (response.data.success) {
+            console.log("Login successful!");
+            // Perform the necessary actions after successful login
+          } else {
+            console.log("Login failed!");
+            // Perform the necessary actions after failed login
+          }
+        })
+        .catch((error) => {
+          console.error("An error occurred during login:", error);
+          // Handle the login error
+        });
+    }
+  };
+
+const validatePhoneNumber = (phone) => {
+  // Remove any non-digit characters from the phone number
+  const cleanPhoneNumber = phone.replace(/\D/g, "");
+
+  // Check if the phone number is greater than ten digits and contains only digits
+  if (cleanPhoneNumber.length > 10 && /^\d+$/.test(cleanPhoneNumber)) {
+    return true; // Valid phone number
+  } else {
+    return false; // Invalid phone number
+  }
+};
+
+
+
   return (
-    <div className="bg-gray-100 h-[100vh]  flex flex-col items-center justify-center gap-5">
-      <div className="flex gap-5">
-        <img src={logo} alt="logo" height={30} width={30} />
-        <span className="text-green-300 italic font-bold">Equb System</span>
-      </div>
-      <div className="p-[10px]">
-        <span className="text-black text-[24px] font-bold">
-          Sign in to your account
-        </span>
-      </div>
-      <div className="flex justify-center items-center bg-white rounded-lg text-black shadow-xl p-14">
-        <form
-          action=""
-          className="flex justify-center flex-col items-center w-full gap-8 px-4"
-        >
-          <div className="flex justify-start items-left flex-col gap-[10px]  ">
-            <label htmlFor="phone">Phone Number</label>
-            <input
-              type="tel"
-              className="w-[350px] h-8 bg-white border-2 rounded-md border-gray-300 outline-none"
-            />
-          </div>
-          <div className="flex justify-start items-left flex-col gap-[10px]  ">
-            <label htmlFor="phone">Password</label>
-            <input
-              type="password"
-              className="w-[350px] h-8 bg-white border-2 rounded-md border-gray-300 outline-none"
-            />
-          </div>
-          <div className="flex justify-between items-center gap-[90px]">
-            <div className="flex gap-3">
-              <input type="checkbox" />
-              <span>Remember me</span>
+    <div className="flex justify-center items-center h-screen border-2 bg-gray-500">
+      <div className="border-2  p-4">
+        <form className="-mx-2" onSubmit={handleLogin}>
+          <h2 className="text-2xl mb-6 font-serif font-bold">Login</h2>
+          {!isForgotPassword && (
+            <div className="mb-4">
+              <label
+                className="block text-white-700 text-sm font-bold mb-2"
+                htmlFor="phone"
+              >
+                Phone Number:
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-white-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                id="phone"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+              />
             </div>
-            <div>
-              <Link to="/forget" className="text-blue-400">Forgot password ?</Link>
+          )}
+          {!isForgotPassword && (
+            <div className="mb-4">
+              <label
+                className="block text-white-700 text-sm font-bold mb-2"
+                htmlFor="password"
+              >
+                Password:
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-white-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
-          </div>
-          <div>
-            <button type="submit" className="w-[350px] bg-blue-700 h-8 text-center rounded-md text-white hover:bg-blue-500">Login</button>
-          </div>
-          <div>
-            <span>Have't an account ? {" "}</span>
-            <Link to="/signup" className="text-blue-500">Register</Link>
-          </div>
+          )}
+          {isForgotPassword && (
+            <div className="mb-4">
+              <label
+                className="block text-white-700 text-sm font-bold mb-2"
+                htmlFor="email"
+              >
+                Email:
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-white-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+          )}
+          {errorMessage && (
+            <p className="text-red-400 text-sm mb-4">{errorMessage}</p>
+          )}
+          {!isForgotPassword && (
+            <div className="flex items-center">
+              <input
+                className="mr-2 leading-tight"
+                type="checkbox"
+                id="forgotPassword"
+                checked={isForgotPassword}
+                onChange={() => setIsForgotPassword(!isForgotPassword)}
+              />
+              <label className="text-sm" htmlFor="forgotPassword">
+                Forgot password?
+              </label>
+            </div>
+          )}
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Login
+          </button>
         </form>
       </div>
     </div>
@@ -56,5 +143,3 @@ const Login = () => {
 };
 
 export default Login;
-=======
->>>>>>> gebre
