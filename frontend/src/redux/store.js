5279391b78330bc331e7e82a99855b45d29cacce
BDU120314 <tsecha6@gmail.com>
2023-06-15 00:07:25 +0300

    update code

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,13 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import loginReducer from "./reducers/loginReducer";
-import authReducer from "./reducers/authReducer";
-import equbTypeReducer from "./reducers/equbTypeReducer"
 
 const store = configureStore({
   reducer: {
-    login: loginReducer,
-    auth: authReducer,
-    equb: equbTypeReducer,
+   
   },
 });
 
